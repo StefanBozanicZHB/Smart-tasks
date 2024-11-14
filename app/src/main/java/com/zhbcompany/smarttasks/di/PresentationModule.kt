164190e3b9219a106cde70aa43efa8ac9a833754
Stefan Božanić <stefan.bozanic@computerrock.com>
2024-11-14 23:56:27 +0100

    #16 Provide TaskDetailsViewModel in PresentationModule

--- app/src/main/java/com/zhbcompany/smarttasks/di/PresentationModule.kt
+++ app/src/main/java/com/zhbcompany/smarttasks/di/PresentationModule.kt
@@ -1,6 +1,7 @@
 package com.zhbcompany.smarttasks.di
 
 import com.zhbcompany.smarttasks.domain.use_case.TaskUseCases
+import com.zhbcompany.smarttasks.presentation.task_details.TaskDetailsViewModel
 import com.zhbcompany.smarttasks.presentation.task_list.TaskListViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -27,6 +28,23 @@ private fun provideTaskListViewModel(
     return TaskListViewModel(uceCase, dispatcher)
 }
 
+/**
+ * Creates and provides an instance of [TaskDetailsViewModel].
+ *
+ * This function is responsible for creating a new instance of [TaskDetailsViewModel] by injecting
+ * the required dependencies: [TaskUseCases] and [CoroutineDispatcher].
+ *
+ * @return An instance of [TaskDetailsViewModel] configured with the provided dependencies. The returned view model
+ * can be used to handle user interactions and update the UI based on the task details data.
+ */
+private fun provideTaskDetailsViewModel(
+    uceCase: TaskUseCases,
+    dispatcher: CoroutineDispatcher,
+): TaskDetailsViewModel {
+    return TaskDetailsViewModel(uceCase, dispatcher)
+}
+
 val presentationModule: Module = module {
     viewModel { provideTaskListViewModel(get(), get(named("IoDispatcher"))) }
+    viewModel { provideTaskDetailsViewModel(get(), get(named("IoDispatcher"))) }
 }
